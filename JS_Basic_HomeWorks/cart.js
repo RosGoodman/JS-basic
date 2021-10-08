26a2db55f158698c8817458448606331386b2e93
RosGoodman <pavel.official@bk.ru>
2021-10-08 12:59:14 +0300

    chrome err

---
+++ JS_Basic_HomeWorks/cart.js
@@ -0,0 +1,19 @@
+"use strict"
+
+/*class Cart {
+   prodList = [];
+
+   constructor() { }
+
+   addProduct(product) {
+      this.prodList.push(product);
+   }
+
+   removeProduct(product) {
+      for (let i = 0; i < this.prodList.length(); i++) {
+         if (prodList[i] == product.prodName) {
+            delete this.prodList[i];
+         }
+      }
+   }
+}*/
---
+++ JS_Basic_HomeWorks/product.js
@@ -0,0 +1,12 @@
+"use strict"
+
+/*class Product {
+
+   prodName = "";
+   prodPrice = 0;
+
+   constructor(name, price) {
+      this.prodName = name;
+      this.prodPrice = price;
+   }
+}*/
